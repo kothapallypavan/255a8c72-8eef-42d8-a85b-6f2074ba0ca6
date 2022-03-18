2ac1b1d9c14576d622ef815280eeb93c0dc9955c
kothapallypavan <kothapallypavan9100@gmail.com>
2022-03-18 06:05:13 +0000

    second commit

--- reactapp/src/signup.js
+++ reactapp/src/signup.js
@@ -28,12 +28,7 @@ function Signup() {
         </h1>
       <form class="log-form" onSubmit={handleSubmit}>
         <div class="login-content">
-          <input type="email" name="email" id="email"  placeholder="Enter email" value={inputs.email} onChange={handleChange}></input><br></br>
-          <input type="text" name="username" id="username" placeholder="Enter Username" value={inputs.username} onChange={handleChange}></input>
-          <input type="text" name="mobileNumber" id="mobileNumber" placeholder="Enter Mobilenumber" value={inputs.mobilenumber} onChange={handleChange}></input>
-          <input type="password" name="password" id="password" placeholder="Password" value={inputs.password} onChange={handleChange}></input>
-          <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password" value={inputs.confirmpassword} onChange={handleChange}></input>
-        </div>
+          </div>
         <div class='singup-link'>
         <p>Already a User?<a onClick={() => {
               navigate("/");
