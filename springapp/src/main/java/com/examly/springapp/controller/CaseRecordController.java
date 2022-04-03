7941d77b6e8af50f0ba04b2b598b492678e93b78
kothapallypavan <kothapallypavan9100@gmail.com>
2022-04-03 19:42:33 +0000

    2 TC passes

--- springapp/src/main/java/com/examly/springapp/controller/CaseRecordController.java
+++ springapp/src/main/java/com/examly/springapp/controller/CaseRecordController.java
@@ -1,4 +1,6 @@
 package com.examly.springapp.controller;
+import java.util.Map;
+
 import com.examly.springapp.Model.CaseRecordModel;
 import com.examly.springapp.Model.Record;
 import com.examly.springapp.Repositorie.UserRepository;
@@ -6,10 +8,12 @@ import com.examly.springapp.Repositorie.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +29,7 @@ public class CaseRecordController{
     public RecordRepository RR;
 
     //Add case record
+    @CrossOrigin(origins = "https://8081-babeffbeddcfcbbecbcefddccbedbddd.examlyiopb.examly.io")
     @RequestMapping(method = RequestMethod.POST,value="/Lawyer/Case Record")
     public int addcaserecord(@RequestBody CaseRecordModel ll){
         Record r = new Record();
@@ -37,6 +42,7 @@ public class CaseRecordController{
     } 
 
     //update case record
+    @CrossOrigin(origins = "https://8081-babeffbeddcfcbbecbcefddccbedbddd.examlyiopb.examly.io")
     @PutMapping(path="/Lawyer/Case Record")
     public int updaterecord(@RequestBody CaseRecordModel ll){
         Record old = RR.findByCID(ll.getCaseID());
@@ -47,14 +53,17 @@ public class CaseRecordController{
     }
 
     //Delete Case Record
+    @CrossOrigin(origins = "https://8081-babeffbeddcfcbbecbcefddccbedbddd.examlyiopb.examly.io")
     @DeleteMapping(path="/Lawyer/Case Record")
-    public @ResponseBody int delete_case(@RequestBody CaseRecordModel ll){
-        Record old = RR.findByCID(ll.getCaseID());
+    public @ResponseBody int delete_case(@RequestParam("CaseRecordID") String itemid){
+        Record old = RR.findByCID(itemid);
         RR.deleteById(old.id());
+        System.out.println(itemid);
         return 1;
     }
 
     //update event and action
+    @CrossOrigin(origins = "https://8081-babeffbeddcfcbbecbcefddccbedbddd.examlyiopb.examly.io")
     @PutMapping(path="/actionevent")
     public int updateAE(@RequestBody Record ll){
         Record old = RR.findBybooking_id(ll.getBookingid());
