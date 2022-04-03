5711907c282eaf25a5d903f7e343b0926f8a8fb3
sandireddybhavana <sandireddyb13@gmail.com>
2022-04-03 10:36:12 +0000

    installed strings

--- springapp/src/main/java/com/examly/springapp/Model/BookingModel.java
+++ springapp/src/main/java/com/examly/springapp/Model/BookingModel.java
@@ -1,3 +1,29 @@
+package com.examly.springapp.Model;
+
+import java.util.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BookingModel {
-    
+
+     @JsonProperty("bookingid")
+     private String bookingid;
+
+     private UserModel clientDetail;
+     
+     @JsonProperty("lawfirmName")
+     private String lawfirmName;
+     
+     private Date date;
+     private Date time;
+
+     @JsonProperty("bookingStatus")
+     private boolean bookingStatus;
+
+
+     public BookingModel() {
+     }
+     
+     
 }
