89f434f95390bae97f3a9e57bc972f950e2394f4
kothapallypavan <kothapallypavan9100@gmail.com>
2022-04-03 05:06:19 +0000

    lawyerreport.js

--- reactapp/src/components/lawyerreport.js
+++ reactapp/src/components/lawyerreport.js
@@ -139,11 +139,11 @@ const Lawerreport = () => {
         <Container>
           <h2>LawHarbor</h2>            
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
+              <Navbar.Collapse id="basic-navbar-nav"><Nav className="me-auto">
      
      </Nav>
-     <Nav style={{float:"right"}}><Nav.Link onClick={homelink}>Home</Nav.Link>
+   <Nav style={{float:"right"}}>
+                  <Nav.Link onClick={homelink}>Home</Nav.Link>
                   <Nav.Link onClick={goback}>CaseRecord</Nav.Link>
                   <Nav.Link onClick={reportlink}>Report</Nav.Link>
                   <Nav.Link onClick={constForm}>Logout</Nav.Link>
