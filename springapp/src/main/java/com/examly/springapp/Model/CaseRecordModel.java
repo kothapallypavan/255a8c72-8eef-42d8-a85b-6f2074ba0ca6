16189ae0976560d65ca47ca222536def934f03eb
kothapallypavan <kothapallypavan9100@gmail.com>
2022-04-03 10:41:10 +0000

    variables and methods

--- springapp/src/main/java/com/examly/springapp/Model/CaseRecordModel.java
+++ springapp/src/main/java/com/examly/springapp/Model/CaseRecordModel.java
@@ -1,3 +1,58 @@
+package com.examly.springapp.Model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CaseRecordModel {
     
+    @JsonProperty("CaseRecordID")
+     private String CaseRecordID;
+     
+     private UserModel userId;
+
+     @JsonProperty("date")
+     private String date;
+     
+     @JsonProperty("eventDetail")
+     private String eventDetail;
+
+     @JsonProperty("actionTaken")
+     private String actionTaken;
+
+     private UserModel issuedBy;
+
+
+     public CaseRecordModel() {
+     }
+     public CaseRecordModel(String CaseRecordID,UserModel userId,String date,String eventDetail,String actionTaken,UserModel issuedBy){
+         super();
+         this.CaseRecordID=CaseRecordID;
+         this.userId=userId;
+         this.date=date;
+         this.eventDetail=eventDetail;
+         this.actionTaken=actionTaken;
+         this.issuedBy=issuedBy;
+     }
+     public String getCaseID(){
+         return CaseRecordID;
+     }
+     public UserModel getUserid(){
+        return userId;
+    }
+    public String getDate(){
+        return date;
+    }
+    public String getEvent(){
+        return eventDetail;
+    }
+    public String getAction(){
+        return actionTaken;
+    }
+    public UserModel getIssue(){
+        return issuedBy;
+    }
+     
 }
