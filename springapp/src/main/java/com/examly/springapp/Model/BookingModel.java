46f99d1b36eef9dd1d5ebd0695a541349932d1ef
kothapallypavan <kothapallypavan9100@gmail.com>
2022-04-05 09:50:13 +0000

    bookingmodel

--- springapp/src/main/java/com/examly/springapp/Model/BookingModel.java
+++ springapp/src/main/java/com/examly/springapp/Model/BookingModel.java
@@ -7,7 +7,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BookingModel {
 
-     @JsonProperty("bookingid")
+
+    @JsonProperty("bookingid")
      private String bookingid;
 
      private UserModel clientDetail;
@@ -21,6 +22,8 @@ public class BookingModel {
      @JsonProperty("bookingStatus")
      private boolean bookingStatus;
 
+    
+
 
      public BookingModel() {
      }
@@ -33,5 +36,53 @@ public class BookingModel {
          this.time=time;
          this.bookingStatus = bookingStatus;
      }
+
+     public String getBookingid() {
+        return this.bookingid;
+    }
+
+    public void setBookingid(String bookingid) {
+        this.bookingid = bookingid;
+    }
+
+    public UserModel getClientDetail() {
+        return this.clientDetail;
+    }
+
+    public void setClientDetail(UserModel clientDetail) {
+        this.clientDetail = clientDetail;
+    }
+
+    public String getLawfirmName() {
+        return this.lawfirmName;
+    }
+
+    public void setLawfirmName(String lawfirmName) {
+        this.lawfirmName = lawfirmName;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getTime() {
+        return this.time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public boolean isBookingStatus() {
+        return this.bookingStatus;
+    }
+
+    public void setBookingStatus(boolean bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
      
 }
