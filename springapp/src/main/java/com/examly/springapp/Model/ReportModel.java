33acdb7079ec0431dc3c375a4693e23f28d6271e
kothapallypavan <kothapallypavan9100@gmail.com>
2022-04-05 09:10:37 +0000

    reportmodel

--- springapp/src/main/java/com/examly/springapp/Model/ReportModel.java
+++ springapp/src/main/java/com/examly/springapp/Model/ReportModel.java
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ReportModel {
      
-     @JsonProperty("reportId")
+    @JsonProperty("reportId")
      private String reportId;
 
      private BookingModel appointmentDetail;
@@ -21,17 +21,67 @@ public class ReportModel {
      
      private UserModel issuedBy;
 
-
      public ReportModel() {
-     }
-     public ReportModel(String reportId,BookingModel appointmentDetail,CaseRecordModel Case_RecordDetail,Date date,String report,UserModel issuedBy){
-         super();
-         this.reportId=reportId;
-         this.appointmentDetail=appointmentDetail;
-         this.Case_RecordDetail=Case_RecordDetail;
-         this.date=date;
-         this.report=report;
-         this.issuedBy=issuedBy;
-     }
+    }
+    public ReportModel(String reportId,BookingModel appointmentDetail,CaseRecordModel Case_RecordDetail,Date date,String report,UserModel issuedBy){
+        super();
+        this.reportId=reportId;
+        this.appointmentDetail=appointmentDetail;
+        this.Case_RecordDetail=Case_RecordDetail;
+        this.date=date;
+        this.report=report;
+        this.issuedBy=issuedBy;
+    }
+    
+    public String getReportId() {
+        return this.reportId;
+    }
+
+    public void setReportId(String reportId) {
+        this.reportId = reportId;
+    }
+
+    public BookingModel getAppointmentDetail() {
+        return this.appointmentDetail;
+    }
+
+    public void setAppointmentDetail(BookingModel appointmentDetail) {
+        this.appointmentDetail = appointmentDetail;
+    }
+
+    public CaseRecordModel getCase_RecordDetail() {
+        return this.Case_RecordDetail;
+    }
+
+    public void setCase_RecordDetail(CaseRecordModel Case_RecordDetail) {
+        this.Case_RecordDetail = Case_RecordDetail;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getReport() {
+        return this.report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+
+    public UserModel getIssuedBy() {
+        return this.issuedBy;
+    }
+
+    public void setIssuedBy(UserModel issuedBy) {
+        this.issuedBy = issuedBy;
+    }
+
+
+     
      
 }
